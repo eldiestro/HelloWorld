2e61b5781771cd9186f1af9c94832aa02741d9c2
eldiestro <eldiestro171@gmail.com>
2022-09-01 12:43:04 +0200

    subimos cambios HelloWorld

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Enter Key to Exit.");
+            Console.ReadLine();
+        }
+    }
+}
